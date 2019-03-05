df623a1af7cbf7cc9700b08bccac337b9e86f761
arishuynhvan <arishuynhvan@gmail.com>
2019-03-05 19:13:56 +0800

    generic SA and finite difference draft

--- include/SensitivityAnalysis.hpp
+++ include/SensitivityAnalysis.hpp
@@ -1,18 +1,33 @@
+// SensitivityAnalysis: a semi-automatic library for common SA methods
+// Copyright (C) 2012- Dr Martin Buist & Dr Alberto Corrias
+// Contacts: martin.buist _at_ nus.edu.sg; alberto _at_ nus.edu.sg
+//
+// See the 'Contributors' file for a list of those who have contributed
+// to this work.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 #ifndef SENSITIVITYANALYSIS_HPP_
 #define SENSITIVITYANALYSIS_HPP_
 
 /**
- * \mainpage Welcome to the Sample Project
+ * \mainpage Welcome to the Sensitivity Analysis library
  *
- * Include a nice description of your project here, in a doxygen format
- *
- * You should also include the headers you need if you are building your
- * project as a library
  *
  * Contact:
+ *   - Huynh Van Tu An a0127826 _at_ u.nus.edu
  *   - Dr Martin Buist martin.buist _at_ nus.edu.sg
  *   - Dr Alberto Corrias alberto _at_ nus.edu.sg
- *   - YOUR NAME
  */
+
+#include "FiniteDifference.hpp"
 
 #endif
