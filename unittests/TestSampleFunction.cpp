46b03608ee97cfced304db5855fe4fd5d0ee13c7
arishuynhvan <arishuynhvan@gmail.com>
2019-03-26 11:19:16 +0800

    Couldn't compile the valid unittest case of finitedifference for the 2D linear model

--- unittests/TestSampleFunction.cpp
+++ unittests/TestSampleFunction.cpp
@@ -1,27 +1,29 @@
-#include "SampleFunction.hpp"
 #include "UnitTest++.h"
+#include "SensitivityAnalysis.hpp"
+#include "Unfit.hpp"
+#include "LinearModel.hpp"
+#include "FiniteDifference.hpp"
 
 namespace SensitivityAnalysis
 {
 namespace UnitTests
 {
-SUITE(UnitTestSampleFunction)
+SUITE(UnitTestFiniteDifference)
 {
-TEST(SampleFunction_TestName1)
+TEST(FiniteDifference_LinearModel_AllArgumentsValid)
 {
-  int x = 0;
-  auto rc = SampleFunction(x);
-
-  CHECK_EQUAL(0, rc);
+    std::vector<std::vector<double>> x {{0.498531, 0.622145, 0.746551, 0.899687,
+      0.995019, 1.24803, 1.49695, 1.7464, 1.86737, 1.92478, 2.07206, 2.12789,
+      2.23212}};
+    std::vector<double> params {{12.0, 1.0}};
+    double d = 0.01;
+    Unfit::Examples::LinearModel lm;
+    FiniteDifference fd;
+    fd.calculateSensitivity(params, x,lm,d);
+    auto meanSC = fd.getSCMean();
+    for (int i=0; i<meanSC.size()-1; i++)  CHECK_CLOSE(meanSC[i], meanSC[i+1],0.0001);
 }
 
-TEST(SampleFunction_TestName2)
-{
-  int x = 1;
-  auto rc = SampleFunction(x);
-
-  CHECK_EQUAL(1, rc);
-}
-}  // suite UnitTestSampleFunction
+}  // suite UnitTestFiniteDifference
 }  // namespace UnitTests
 }  // namespace SensitivityAnalysis
