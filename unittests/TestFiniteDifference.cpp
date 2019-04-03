5de867e5d0410b652a04be40b2e46b3173924b8e
arishuynhvan <arishuynhvan@gmail.com>
2019-04-03 09:38:35 +0800

    Added Test for Inifinity Output but it is not failing correctly

--- unittests/TestFiniteDifference.cpp
+++ unittests/TestFiniteDifference.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <stdexcept>
+#include <limits>
 #include "UnitTest++.h"
 #include "LinearModel.hpp"
 #include "FiniteDifference.hpp"
@@ -10,7 +11,7 @@ namespace UnitTests
 {
 SUITE(UnitTestFiniteDifference)
 {
-TEST(FiniteDifference_LinearModel_AllArgumentsValid)
+TEST(LinearModel_AllArgumentsValid)
 {
   std::vector<std::vector<double>> x {{0.498531, 0.622145, 0.746551, 0.899687,
   0.995019, 1.24803, 1.49695, 1.7464, 1.86737, 1.92478, 2.07206, 2.12789,
@@ -29,7 +30,7 @@ TEST(FiniteDifference_LinearModel_AllArgumentsValid)
   CHECK_EQUAL(minSC.size(), params.size());
   CHECK_EQUAL(maxSC.size(), params.size());
 }
-TEST(FiniteDifference_ZeroDifference)
+TEST(ZeroDifference)
 {
   std::vector<std::vector<double>> x {{0.498531, 0.622145, 0.746551, 0.899687,
   0.995019, 1.24803, 1.49695, 1.7464, 1.86737, 1.92478, 2.07206, 2.12789,
@@ -37,9 +38,18 @@ TEST(FiniteDifference_ZeroDifference)
   std::vector<double> params {{12.0, 1.0}};
   double d = 0.0;
   Unfit::Examples::LinearModel lm;
-
   FiniteDifference fd;
-  ;
+  CHECK_THROW(fd.calculateSensitivity(params, x,lm,d), std::runtime_error);
+}
+TEST(InfiniteOutputs)
+{
+  std::vector<std::vector<double>> x {{-std::numeric_limits<double>::infinity(), 0.622145, 0.746551, 0.899687,
+  0.995019, 1.24803, 1.49695, 1.7464, 1.86737, 1.92478, 2.07206, 2.12789,
+  std::numeric_limits<double>::infinity()}};
+  std::vector<double> params {{12.0, 1.0}};
+  double d = 0.0;
+  Unfit::Examples::LinearModel lm;
+  FiniteDifference fd;
   CHECK_THROW(fd.calculateSensitivity(params, x,lm,d), std::runtime_error);
 }
 }  // suite UnitTestFiniteDifference
